45cb3b8ad00f58343e30deb32ae900cc7e55d0a6
Guillermo Iguaran <guilleiguaran@gmail.com>
2014-04-14 02:19:25 -0500

    First example
    Simple app that uses views, params and helpers

---
+++ example1/app.rb
@@ -0,0 +1,18 @@
+require 'nancy'
+require 'nancy/render'
+
+module Helpers
+  def greeting name
+    "Howdy #{name}"
+  end
+end
+
+class App < Nancy::Base
+  include Nancy::Render
+  include Helpers
+
+  get '/:name' do
+    @message = greeting params["name"]
+    render "hello.erb"
+  end
+end
